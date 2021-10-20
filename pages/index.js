2269f23def2c05e26bc28922140a084588aa4297
Edward Reed II <edwardareed@gmail.com>
2021-10-19 19:34:53 -0500

    Init Mock commit

--- pages/index.js
+++ pages/index.js
@@ -4,65 +4,72 @@ export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <Head>
-        <title>Create Next App</title>
+        <title>Mysilio Gate</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
+      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 pb-40 text-center">
         <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
+          Greetings, Traveler 
         </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
-            pages/index.js
-          </code>
-        </p>
+        <span className="text-sm">
+        Varius facilisi mauris sed sit. Non sed et duis dui leo, vulputate id malesuada non. Cras aliquet purus dui laoreet diam sed lacus, fames. Dui, amet, nec sit pulvinar.
+        </span>
 
         <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
           <a
             href="https://nextjs.org/docs"
             className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
           >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
+            <h3 className="text-2xl font-bold">A list of people I’ll probably never meet whose names I would intentionally mispronounce as a flex </h3>
             <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
+            Varius facilisi mauris sed sit. Non sed et duis dui leo, vulputate id malesuada non. Cras aliquet purus dui laoreet diam sed lacus, fames.
             </p>
+            <>
+            Created 4/24/21 Last Edited 4:22 PM 5/19/21
+            </>
+            <button className="">
+              Read More
+            </button>
           </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+        </div>
+        <div className="flex flex-col text-left mt-16">
+          <h1 className="text-5xl font-bold">
+            Check out my other shit 
+          </h1>
+          <span className="text-sm">
+          Varius facilisi mauris sed sit. Non sed et duis dui leo, vulputate id malesuada non. Cras aliquet purus dui laoreet diam sed lacus, fames. Dui, amet, nec sit pulvinar.
+          </span>
+          <button className="">
+            Subscribe
+          </button>
+          <>
+            <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
+              <a
+                href="https://nextjs.org/docs"
+                className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
+              >
+                <h3 className="text-2xl font-bold">My Etsy Store</h3>
+                <button className="">
+                 Visit 
+                </button>
+              </a>
+            </div>
+            <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
+              <a
+                href="https://nextjs.org/docs"
+                className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
+              >
+                <h3 className="text-2xl font-bold">My Amazon Wishlist</h3>
+                <button className="">
+                 Visit 
+                </button>
+              </a>
+            </div>
+          </>
+          <>
+            facebook insta twitta github dribble
+          </>
         </div>
       </main>
 
@@ -73,9 +80,11 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
+          Powered by Mysilio
         </a>
+        <button>
+          Start Building Now
+        </button>
       </footer>
     </div>
   )
