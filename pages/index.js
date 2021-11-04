ce5392ddcaa22d015d3ccbeab89fe9a28f539741
Edward Reed II <edwardareed@gmail.com>
2021-11-04 17:30:53 -0500

    Add Button Icons

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import Head from 'next/head'
 import Image from 'next/image';
-import { FacebookIcon, InstagramIcon, TwitterIcon, GithubIcon, DribbleIcon } from '../components/icons' 
+import { Logo } from '../components/logo';
+import { SubscribeIcon, FacebookIcon, InstagramIcon, TwitterIcon,
+         EditIcon, LinkIcon, GithubIcon, DribbleIcon } from '../components/icons' 
 
 let headerImage = "http://picsum.photos/1360/544";
 let profileImage = "http://picsum.photos/176/176";
@@ -15,11 +17,11 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="flex flex-col items-center w-full flex-1 text-center">
+      <main className="flex flex-col overflow-hidden items-center w-full flex-1 text-center">
         <div className="flex flex-row relative items-stretch w-screen h-96"> 
           <Image className="z-0 object-cover h-96" src={headerImage} alt="forest" layout="fill" /> 
         </div>
-        <div className="flex flex-col w-7/12 text-left -mt-20 bg-white rounded-2xl shadow  z-20  py-12 px-14 pl-20">
+        <div className="flex flex-col w-7/12 text-left -mt-20 bg-white rounded-2xl shadow z-20 py-12 px-14 pl-20">
           <div className="flex absolute -ml-52 -mt-6 rounded-full border-white border-8 shadow-lg">
             <Image className="z-0 rounded-full" src={profileImage} width="176" height="176"/> 
           </div>
@@ -44,8 +46,8 @@ export default function Home() {
               <div className="py-3 text-gray-400">
                 <bold className="text-my-green font-bold">Created</bold> 4/24/21 <bold className="text-my-green font-bold ml-2">Last Edited</bold> 4:22 PM 5/19/21
               </div>
-              <button className="mt-4  w-36 py-2 rounded border border-my-green text-center text-my-green">
-                Read More
+              <button className="flex flex-row mt-4 w-36 pl-4 pt-4 py-2 rounded border border-my-green text-center text-my-green">
+                Read More <LinkIcon className="ml-2 -mt-1 h-8"/>
               </button>
             </div>
           </div>
@@ -58,8 +60,8 @@ export default function Home() {
               <div className="py-3 text-gray-400">
                 <bold className="text-my-green font-bold">Created</bold> 4/24/21 <bold className="text-my-green font-bold ml-2">Last Edited</bold> 4:22 PM 5/19/21
               </div>
-              <button className="mt-4 w-36 py-2 rounded border border-my-green text-center text-my-green">
-                Read More
+              <button className="flex flex-row mt-4 w-36 pl-4 pt-4 py-2 rounded border border-my-green text-center text-my-green">
+                Read More <LinkIcon className="ml-2 -mt-1 h-8"/>
               </button>
             </div>
             <div className="flex-none">
@@ -75,8 +77,8 @@ export default function Home() {
             <span className="text-sm mt-4 opacity-80">
             Varius facilisi mauris sed sit. Non sed et duis dui leo, vulputate id malesuada non. Cras aliquet purus dui laoreet diam sed lacus, fames. Dui, amet, nec sit pulvinar.
             </span>
-            <button className="mt-10 text-center w-36 py-2 rounded-full border-2 bg-white bg-opacity-20">
-              Subscribe
+            <button className="flex flex-row mt-10 px-6 pt-4 py-1.5 text-center w-40 rounded-full border-2 bg-white bg-opacity-20">
+              Subscribe <SubscribeIcon className="ml-2 -mt-1 h-10"/> 
             </button>
           </div>
           <div className="flex flex-row">
@@ -91,7 +93,7 @@ export default function Home() {
                 <a
                   href="https://nextjs.org/docs"
                   className="ml-5">
-                Visit 
+                Visit → 
                 </a>
               </div>
             </div>
@@ -106,7 +108,7 @@ export default function Home() {
                 <a
                   href="https://nextjs.org/docs"
                   className="ml-5">
-                Visit 
+                Visit → 
                 </a>
               </div>
             </div>
@@ -121,7 +123,7 @@ export default function Home() {
                 <a
                   href="https://nextjs.org/docs"
                   className="ml-5">
-                Visit 
+                Visit → 
                 </a>
               </div>
             </div>
@@ -136,44 +138,45 @@ export default function Home() {
                 <a
                   href="https://nextjs.org/docs"
                   className="ml-5">
-                Visit 
+                Visit → 
                 </a>
               </div>
             </div>
           </div>
-          <div className="flex flex-row p-20">
-            <div className="h-12 w-12">
+          <div className="flex flex-row p-24">
+            <div className="h-12 w-12 ml-12 transform scale-150">
               <FacebookIcon /> 
             </div>
-            <div className="h-12 w-12 ml-12">
+            <div className="h-12 w-12 ml-12 transform scale-150">
               <InstagramIcon /> 
             </div>
-            <div className="h-12 w-12 ml-12">
+            <div className="h-12 w-12 ml-12 transform scale-150">
               <TwitterIcon /> 
             </div>
-            <div className="h-12 w-12 ml-12">
+            <div className="h-12 w-12 ml-12 transform scale-150">
               <GithubIcon /> 
             </div>
-            <div className="h-12 w-12 ml-12">
+            <div className="h-12 w-12 ml-12 transform scale-150">
               <DribbleIcon /> 
             </div>
           </div>
+          <footer className="flex items-center justify-center text-white p-10 bg-white bg-opacity-10 backdrop-blur-sm w-full">
+            <Logo className="h-16"/>
+            <a
+              className="flex ml-5 items-center justify-center font-bold text-xl"
+              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Powered by Mysilio
+            </a>
+            <button className="flex flex-row pt-4 text-center ml-5 py-2 px-6 rounded-full border-2 bg-white bg-opacity-20">
+              Start Building Now <EditIcon  className="ml-2 -mt-1 h-9"/>
+            </button>
+          </footer>
         </div>
       </main>
 
-      <footer className="flex items-center justify-center w-full h-24 border-t">
-        <a
-          className="flex items-center justify-center"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by Mysilio
-        </a>
-        <button>
-          Start Building Now
-        </button>
-      </footer>
     </div>
   )
 }
